1eed9d137b2b5a12c49989fa9e3898dc1fef9fa9
Andrew Stakhov <andrew.stakhov@gmail.com>
2019-03-15 15:27:32 -0400

    Initial

--- DateTimeNowPatch/Program.cs
+++ DateTimeNowPatch/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+using Harmony;
 
 namespace DateTimeNowPatch
 {
@@ -10,6 +10,27 @@ namespace DateTimeNowPatch
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Before patch: {DateTime.Now}");
+            var harmony = HarmonyInstance.Create("com.github.harmony.rimworld.mod.example");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Console.WriteLine($"After patch: {DateTime.Now}");
+            Console.ReadLine();
         }
+    }
+
+    [Harmony]
+    class Patch
+    {
+        static MethodBase TargetMethod() => AccessTools.Property(typeof(DateTime), nameof(DateTime.Now)).GetMethod;
+        
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr) =>
+            new[]
+            {
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Patch), nameof(MyCustomDateTime))),
+                new CodeInstruction(OpCodes.Ret)
+            };
+
+        public static DateTime MyCustomDateTime() =>
+            TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time"));
     }
 }
